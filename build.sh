d085de185c93f5883b06ec55f3d529f5e8378c0e
Robbe Derks <robbe.derks@gmail.com>
2020-07-27 02:22:23 +0200

    Initial implementation of ADD

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-cd data
+
+GHIDRA_PATH=$HOME/ghidra/Ghidra
+EXTENSION_PATH=$GHIDRA_PATH/Processors/C166V2
+
+
+rm -r $EXTENSION_PATH/*
+cp -r * $EXTENSION_PATH/.
+
+cd $EXTENSION_PATH/data
 ant
